43a4c85c51b4ca6f87504cd19c2fa3ca26885509
Arturo J Gallardo Ruvalcaba <arturogallardojr@hotmail.com>
2020-09-21 17:52:47 -0500

    Ahora es una suma

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def print_hi(name):
-    print(f'Hola, {name}')
+def suma():
+    num1 = int(input("Dame un número:"))
+    num2 = int(input("Dame otro número:"))
+    print("La suma es: ", num1 + num2)
 
 
 if __name__ == '__main__':
-    print_hi('Arturo')
-
+    suma()
