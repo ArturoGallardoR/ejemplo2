55e2e09e9d881cc0829a24ac2224a4600137c980
Arturo J Gallardo Ruvalcaba <arturogallardojr@hotmail.com>
2020-09-21 17:54:09 -0500

    Ajustando espacios

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def suma():
-    num1 = int(input("Dame un número:"))
-    num2 = int(input("Dame otro número:"))
-    print("La suma es: ", num1 + num2)
+    num1 = int(input("Dame un número: "))
+    num2 = int(input("Dame otro número: "))
+    print("La suma es:", num1 + num2)
 
 
 if __name__ == '__main__':
